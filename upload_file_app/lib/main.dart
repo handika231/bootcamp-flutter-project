26ae4d8867003345928ccc45c62e693c4af17d75
Handika Rafli A <shandika231@gmail.com>
2022-10-23 14:34:22 +0700

    add utils

--- upload_file_app/lib/main.dart
+++ upload_file_app/lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:upload_file_app/application/application.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const Application());
 }
---
+++ upload_file_app/lib/utils/upload_helper.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/services.dart';
+
+class UploadHelper {
+  static UploadTask? uploadFile(String destination, File file) {
+    try {
+      final Reference ref = FirebaseStorage.instance.ref(destination);
+      return ref.putFile(file);
+    } on FirebaseException {
+      return null;
+    }
+  }
+
+  static UploadTask? uploadBytes(String destination, Uint8List data) {
+    try {
+      final Reference ref = FirebaseStorage.instance.ref(destination);
+      return ref.putData(data);
+    } on FirebaseException {
+      return null;
+    }
+  }
+}
